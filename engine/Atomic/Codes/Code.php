519aae0e46b9621bfaa7d3f405525fc1429c9264
LukashevychAndrii <andriilukashevych@gmail.com>
2026-04-03 21:46:08 +0000

    8.1 trait const

--- engine/Atomic/Codes/Code.php
+++ engine/Atomic/Codes/Code.php
@@ -6,6 +6,20 @@ if (!defined( 'ATOMIC_START' ) ) exit;
 
 class Code
 {
-    use OAuth;
-    use Generic;
+    // Generic
+    public const SUCCESS = '200';
+    public const BAD_REQUEST = '400';
+    public const UNAUTHORIZED = '401';
+    public const FORBIDDEN = '403';
+    public const RATE_LIMIT = '429';
+    public const NONCE_INVALID = '440';
+    public const SERVER_ERROR = '500';
+    public const SERVICE_UNAVAILABLE = '503';
+
+    // OAuth
+    public const OAUTH_TOKEN_ERROR = '450';
+    public const OAUTH_USER_DATA_ERROR = '451';
+    public const OAUTH_ACCOUNT_ALREADY_LINKED = '452';
+    public const OAUTH_NOT_CONFIGURED = '453';
+    public const OAUTH_INVALID_STATE = '454';
 }
