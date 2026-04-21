00809074817170292ba1356d15d9bc6e934ec0a5
LukashevychAndrii <andriilukashevych@gmail.com>
2026-04-21 21:17:57 +0000

    fix order

--- engine/Atomic/Queue/Drivers/lua/load_events.lua
+++ engine/Atomic/Queue/Drivers/lua/load_events.lua
@@ -15,17 +15,11 @@ end
 
 local batches = cjson.decode(batches_json)
 
-for batch_index = #batches, 1, -1 do
+for batch_index = 1, #batches do
     local batch_uuid = batches[batch_index]
     local batch_key = prefix .. 'telemetry.batch.' .. batch_uuid
     local batch_events = redis.call('LRANGE', batch_key, 0, -1)
-    local reversed_batch_events = {}
-
-    for event_index = #batch_events, 1, -1 do
-        table.insert(reversed_batch_events, batch_events[event_index])
-    end
-
-    table.insert(results, {batch_uuid, reversed_batch_events})
+    table.insert(results, {batch_uuid, batch_events})
 end
 
 return results
